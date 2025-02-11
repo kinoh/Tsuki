c89231c9c18518c1ce3364f587219450a03180d7
kino <p@kinon.sakura.ne.jp>
2025-02-12 06:36:03 +0900

    dummy patterns for local test

--- src/main/scala/DummyConversationEngine.scala
+++ src/main/scala/DummyConversationEngine.scala
@@ -1,4 +1,27 @@
+import upickle.default.write
+import upickle.default.read
+import scala.util.Try
+
 class DummyConversationEngine extends ConversationEngine {
   def chat(history: Seq[MessageRecord]): MessageRecord =
-    MessageRecord("assistant", "", encodeAssistantMessage(AssistantMessage(0, 0, history.last.message)))
+    val patterns = Map(
+      "#code-time"  -> AssistantCode("import datetime\nprint(datetime.datetime.utcnow())"),
+      "#code-sleep" -> AssistantCode("import time\ntime.sleep(5)\nprint(\"it's time\")"),
+      "#code-req"   -> AssistantCode("import requests\nr = requests.get(\"https://example.com\")\nprint(r.text)"),
+      "#code-err"   -> AssistantCode("print("),
+    )
+
+    val message = patterns.map { (key, value) =>
+      Option.when(history.last.message.contains(key)) {
+        write(value)
+      }
+    }.collectFirst {
+      case Some(value) => value
+    }.orElse(
+      Try(read[UserMessage](history.last.message))
+        .toOption
+        .map(m => write(AssistantMessage(0, 0, m.content)))
+    ).getOrElse(write(AssistantMessage(0, 0, "🤔")))
+
+    MessageRecord("assistant", "", message)
 }
