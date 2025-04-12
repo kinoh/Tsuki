fdfb2564231446da1c2be295818d0e3ef5001f58
kino <p@kinon.sakura.ne.jp>
2025-04-12 14:54:52 +0900

    revert for pc

--- gui/src-tauri/src/lib.rs
+++ gui/src-tauri/src/lib.rs
@@ -2,10 +2,14 @@
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .plugin(tauri_plugin_app_events::init())
         .plugin(tauri_plugin_http::init())
         .plugin(tauri_plugin_fcm::init())
         .plugin(tauri_plugin_notification::init())
+        .setup(|app| {
+            #[cfg(mobile)]
+            app.handle().plugin(tauri_plugin_app_events::init())?;
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
