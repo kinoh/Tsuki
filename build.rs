902c734f44d7cb8edb92128e4d089a66ad0617fc
kino <4271312+kinoh@users.noreply.github.com>
2025-05-07 08:27:05 +0000

    detect openssl dependency

--- build.rs
+++ build.rs
@@ -8,6 +8,15 @@ fn main() {
     let git_hash = String::from_utf8(output.stdout).unwrap();
     println!("cargo:rustc-env=GIT_HASH={}", git_hash);
 
+    let cargo_lock_raw = include_str!("Cargo.lock");
+    if cargo_lock_raw
+        .split("\n")
+        .any(|line| line.starts_with("name = \"openssl\""))
+    {
+        eprintln!("❌ openssl dependency detected");
+        panic!()
+    }
+
     println!("cargo::rustc-link-search=/usr/local/lib/");
     println!("cargo:rerun-if-changed=src/prompt/initial.txt");
 }
