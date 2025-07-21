5446143511952c70131990e9bd797fb0b204eb79
kino <4271312+kinoh@users.noreply.github.com>
2025-07-21 22:40:36 +0900

    refactor: Unused tool

--- core/src/mastra/agents/tsuki.ts
+++ core/src/mastra/agents/tsuki.ts
@@ -1,7 +1,6 @@
 import { openai } from '@ai-sdk/openai'
 import { Agent } from '@mastra/core/agent'
 import { Memory } from '@mastra/memory'
-import { weatherTool } from '../tools/weather-tool'
 
 export const tsuki = new Agent({
   name: 'Tsuki',
@@ -9,6 +8,5 @@ export const tsuki = new Agent({
     You are a chatting agent.
 `,
   model: openai('gpt-4o-mini'),
-  tools: { weatherTool },
   memory: new Memory(),
 })
