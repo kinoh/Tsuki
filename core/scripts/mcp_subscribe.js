fe7952d4dbd43f596a36c24834b13547394fd71d
kino <4271312+kinoh@users.noreply.github.com>
2025-10-14 22:15:58 +0900

    chore(core): test daily schedule

--- core/scripts/mcp_subscribe.js
+++ core/scripts/mcp_subscribe.js
@@ -1,6 +1,7 @@
 // Use Mastra MCPClient.resources.subscribe to subscribe to messages
 
 import { MCPClient } from '@mastra/mcp'
+import fs from 'fs'
 
 async function main() {
   const client = new MCPClient({
@@ -19,11 +20,11 @@ async function main() {
 
   const tools = await client.getTools()
 
-  client.resources.onUpdated('scheduler', (params) => {
+  await client.resources.onUpdated('scheduler', (params) => {
     console.log('Received updated message from scheduler:', params)
   })
 
-  client.resources.subscribe('scheduler', 'fired_schedule://recent').then(obj => {
+  await client.resources.subscribe('scheduler', 'fired_schedule://recent').then(obj => {
     console.log('subscription started')
   })
 
@@ -39,16 +40,33 @@ async function main() {
     console.log(response)
   }
   {
+    const now = new Date()
     const response = await tools['scheduler_set_schedule'].execute({
       context: {
         name: 'test2',
-        time: new Date().toISOString(),
-        cycle: 'once',
+        // HH:MM:SS expected
+        time: `${String(now.getHours()).padStart(2, '0')}:${String(now.getMinutes()).padStart(2, '0')}:${String(now.getSeconds()).padStart(2, '0')}`,
+        cycle: 'daily',
         message: 'Hello hello',
       },
     })
     console.log(response)
   }
+
+  setTimeout(() => {
+
+    console.log('Exiting...')
+    client.disconnect()
+
+    // Remove data_dir
+    fs.rm('/tmp/tsuki_test_scheduler', { recursive: true, force: true }, (err) => {
+      if (err) {
+        console.error('Error removing data_dir:', err)
+      } else {
+        console.log('data_dir removed')
+      }
+    })
+  }, 3000);
 }
 
 main().catch(console.error)
